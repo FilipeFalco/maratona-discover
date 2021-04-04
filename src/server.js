86d7b0716589b0ce43d86668ecafa3c55e8497c4
Filipe <falcoefilipe@hotmail.com>
2021-04-03 21:53:52 -0400

    aula 2

--- src/server.js
+++ src/server.js
@@ -8,9 +8,13 @@ server.set('view engine', 'ejs');
 // habilitar arquivos statics
 server.use(express.static("public"));
 
+// Utilizar o req.body
+server.use(express.urlencoded({ extend: true }));
+
 //routes
 server.use(routes);
 
+// Abrindo uma porta
 server.listen(3000, () => {
     console.log('rodando')
 })
